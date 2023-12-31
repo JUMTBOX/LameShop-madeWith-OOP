4f0a98d3d1d7106db7b9861ae2a9e20c0ff79c1a
JUMTBOX <yjey12@naver.com>
2023-12-31 20:09:40 +0900

    feat. additional drag & drop feature completion

--- project planner/assets/scripts/APP/ProjectList.js
+++ project planner/assets/scripts/APP/ProjectList.js
@@ -30,11 +30,13 @@ export class ProjectsList {
         e.preventDefault();
       }
     });
+
     list.addEventListener("dragover", (e) => {
       if (e.dataTransfer.types[0] === "text/plain") {
         e.preventDefault();
       }
     });
+
     list.addEventListener("dragleave", (e) => {
       if (
         e.relatedTarget.closest(`#${this.projectType}-projects ul`) !== list
@@ -42,16 +44,30 @@ export class ProjectsList {
         list.parentElement.classList.remove("droppable");
       }
     });
+
+    list.addEventListener("drop", (e) => {
+      const prjId = e.dataTransfer.getData("text/plain");
+
+      let click = new Event("click");
+      document
+        .querySelector(`#${prjId}`)
+        .querySelector("button:last-of-type")
+        .dispatchEvent(click);
+
+      list.parentElement.classList.remove("droppable");
+    });
   }
 
   setSwitchHandlerFunc(switchHandlerFunc) {
     this.switchHandler = switchHandlerFunc;
   }
+
   addProject(project) {
     this.projects.push(project);
     DOMHelper.moveElement(project.id, `#${this.projectType}-projects ul`);
     project.update(this.switchProject.bind(this), this.projectType);
   }
+
   switchProject(id) {
     this.switchHandler(this.projects.find((p) => p.id === id));
     this.projects = this.projects.filter((item) => item !== id);
